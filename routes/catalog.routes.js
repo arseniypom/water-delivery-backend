3d0927407371bbfd7b1345a414d3c6f7eefa6746
arseniypom <arpomazkov@bk.ru>
2021-09-16 00:27:46 +0300

    Added query chaining to /api/products endpoint

--- routes/catalog.routes.js
+++ routes/catalog.routes.js
@@ -8,8 +8,38 @@ const router = Router()
 router.get(
   '/products',
   async (req, res) => {
+    const volume = req.query.volume;
+    const minPrice = req.query.minPrice;
+    const maxPrice = req.query.maxPrice;
+    const sortBy = req.query.sortBy;
+    const sortOrder = req.query.order;
+
     try {
-      const products = await Product.find({available: true})
+      const query = Product.find({available: true})
+      if (volume) {
+        query.find({volume})
+      }
+      if (minPrice || maxPrice) {
+        query.find({
+          $and: [
+            {currentPrice: { $gte: minPrice }},
+            {currentPrice: { $lte: maxPrice }}
+          ]
+        })
+      }
+      if (sortBy === 'price') {
+        switch (sortOrder) {
+          case 'asc':
+            query.sort({currentPrice: 'asc'})
+            break;
+          case 'desc':
+            query.sort({$sort: {currentPrice: -1}})
+            break;
+          default:
+            break;
+        }
+      }
+      const products = await query.exec()
       res.json(products)
     } catch (error) {
       res.status(500).json({message: 'Что-то пошло не так, пожалуйста, повторите попытку позже'})
