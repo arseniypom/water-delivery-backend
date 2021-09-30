10ea0b52c87d2c17fab97c3f992569e93d71e735
arseniypom <arpomazkov@bk.ru>
2021-09-30 13:28:28 +0300

    Modified Product schema

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
   title: {type: String, required: true},
   volume: {type: String, required: true},
+  quantityInPack: {type: Number, required: true},
   currentPrice: {type: Number, required: true},
   previousPrice: {type: Number},
   imageName: {type: String, required: true},
