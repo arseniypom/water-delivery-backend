dbfc317d1e4abb6addecef6c184abbb64d4e1099
arseniypom <arpomazkov@bk.ru>
2021-10-18 21:15:31 +0300

    Added auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+
+    if (!token) {
+      return res.status(401).json({message: 'Not authorized'})
+    }
+
+    const decodedToken = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decodedToken
+    next()
+
+  } catch (e) {
+    res.status(401).json({message: 'Not authorized'})
+  }
+}
